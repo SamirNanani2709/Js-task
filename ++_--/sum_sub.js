c42ff013c6c04acc0b56ed1feeb8798920f9eb50
SAMIR <samir.nanani@0xblock.co>
2023-10-13 11:19:18 +0530

    Increment/decrement

---
+++ ++_--/sum_sub.js
@@ -0,0 +1,11 @@
+function addition() {
+    var a = document.getElementById("f1").innerHTML;
+    a++;
+    document.getElementById("f1").innerHTML = a;
+}
+function subtraction() {
+    var b = document.getElementById("f1").innerHTML;
+    b--;
+    document.getElementById("f1").innerHTML = b;
+}
+
